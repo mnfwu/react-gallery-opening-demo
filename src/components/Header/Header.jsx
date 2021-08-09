7de9cd8319c79a9acec0d7db93eefc41610dd4d2
Matthew Wu <mnfwu12@gmail.com>
2021-08-09 09:48:00 +0800

    change header font

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return(
         <header className="pointer-events-none text-8xl text-center absolute top-5 left-1/4 z-10">
-            <span className="font-bold">ANALOGUE</span>
+            <span className="">ANALOGUE</span>
             <span className="font-serif text-7xl">Space</span>
         </header>
     )
