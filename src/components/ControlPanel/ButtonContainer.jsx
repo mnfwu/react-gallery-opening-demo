24761383f64e3612f4097e74b7bcd56914fedab0
Matthew Wu <mnfwu12@gmail.com>
2021-08-12 15:45:36 +0800

    add arrow component to control panel

--- src/components/ControlPanel/ButtonContainer.jsx
+++ src/components/ControlPanel/ButtonContainer.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 import './ControlPanel.css'
+import Arrow from './Arrow'
 
 const ButtonContainer = (props) => {
     const [hideContainer, setHideContainer] = useState(false)
     const [containerPosition, setContainerPosition] = useState('left-0 top-10')
+    const [arrowDirection, setArrowDirection] = useState('left')
     
     const handleClick = () => {
         setHideContainer(!hideContainer);
         hideContainer ? setContainerPosition('left-0 top-10 container-unhide') : setContainerPosition('container-hide top-10')
+        hideContainer ? setArrowDirection('left') : setArrowDirection('right')
     }
 
     let containerStyles = containerPosition + 'transition-settings bg-transparent border border-gray-800 shadow-xl w-48 h-20 absolute left-0 top-32 z-10'
@@ -19,7 +22,9 @@ const ButtonContainer = (props) => {
                     {props.children}
                 </div>
                 <div>
-                    <button onClick={handleClick}>Hide</button>
+                    <button onClick={handleClick}>
+                        <Arrow className="" direction={arrowDirection} />
+                    </button>
                 </div>
             </div>
         </div>
