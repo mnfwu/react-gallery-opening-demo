28ff86cd2d88105892fd940129a4cd75f5224d3c
Matthew Wu <mnfwu12@gmail.com>
2021-08-14 13:17:30 +0800

    button container and btn style adjustments

--- src/components/ControlPanel/NextButton.jsx
+++ src/components/ControlPanel/NextButton.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const NextButton = (props) => {
     return (
-        <div className="px-6 py-2 border border-gray-200 bg-transparent text-gray-200 shadow-md
-        z-10 
+        <div className="ml-4 px-6 py-2 border border-gray-200 bg-transparent text-gray-200 shadow-md
+        z-10 cursor-pointer
         transform transition hover:scale-105"
             onClick={props.next}
             >
