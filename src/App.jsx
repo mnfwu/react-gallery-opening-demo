f342d2062f106ccbf19638a7710c159c41cf9556
Matthew Wu <mnfwu12@gmail.com>
2021-09-24 11:24:58 +0800

    reconfigure buttons, control panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,51 @@
-import React, { useState } from 'react'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import ImageBox from './components/ImageSection/ImageBox'
-import ButtonContainer from './components/ControlPanel/ButtonContainer'
-import NextButton from './components/ControlPanel/NextButton'
-import Modal from './components/Modal/Modal'
-import { Wrapper, ImageContainer} from './components/ImageSection/styles'
-import { Button } from './components/UI/button.js'
+import React, { useState } from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import ImageBox from './components/ImageSection/ImageBox';
+import ButtonContainer from './components/ControlPanel/ButtonContainer';
+import NextButton from './components/ControlPanel/NextButton';
+import AboutButton from './components/ControlPanel/AboutButton';
+import Modal from './components/Modal/Modal';
+import { Wrapper, ImageContainer } from './components/ImageSection/styles';
+import { Button } from './components/UI/button.js';
 
-import './App.css'
+import './App.css';
 
 const matrix = [
-  [0,0], [1,0], [2,0], [3,0], [4,0],
-  [0,1], [1,1], [2,1], [3,1], [4,1],
-  [0,2], [1,2], [2,2], [3,2], [4,2],
-  [0,3], [1,3], [2,3], [3,3], [4,3],
-  [0,4], [1,4], [2,4], [3,4], [4,4]
-]
-
-// const matrix = [
-//   [0,0], [1,0], [2,0], [3,0], [4,0], [5,0], [6,0], [7,0], [8,0],
-//   [0,1], [1,1], [2,1], [3,1], [4,1], [5,1], [6,1], [7,1], [8,1],
-//   [0,2], [1,2], [2,2], [3,2], [4,2], [5,2], [6,2], [7,2], [8,2],
-//   [0,3], [1,3], [2,3], [3,3], [4,3], [5,3], [6,3], [7,3], [8,3],
-//   [0,4], [1,4], [2,4], [3,4], [4,4], [5,4], [6,4], [7,4], [8,4],
-//   [0,5], [1,5], [2,5], [3,5], [4,5], [5,5], [6,5], [7,5], [8,5],
-//   [0,6], [1,6], [2,6], [3,6], [4,6], [5,6], [6,6], [7,6], [8,6],
-//   [0,7], [1,7], [2,7], [3,7], [4,7], [5,7], [6,7], [7,7], [8,7]
-// ]
+  [0, 0],
+  [1, 0],
+  [2, 0],
+  [3, 0],
+  [4, 0],
+  [0, 1],
+  [1, 1],
+  [2, 1],
+  [3, 1],
+  [4, 1],
+  [0, 2],
+  [1, 2],
+  [2, 2],
+  [3, 2],
+  [4, 2],
+  [0, 3],
+  [1, 3],
+  [2, 3],
+  [3, 3],
+  [4, 3],
+  [0, 4],
+  [1, 4],
+  [2, 4],
+  [3, 4],
+  [4, 4],
+];
 
 function App() {
   const [distance, setDistance] = useState(1);
   const [bgColor, setBgColor] = useState(240);
   const [paintingNum, setPaintingNum] = useState(1);
-  const [paintingUrl, setPaintingUrl] = useState('src/components/ImageSection/static/de_kooning1.jpeg');
+  const [paintingUrl, setPaintingUrl] = useState(
+    'src/components/ImageSection/static/de_kooning1.jpeg'
+  );
   const [showModal, setShowModal] = useState(false);
 
   const easing = (num) => Math.pow(num, 3);
@@ -45,84 +57,80 @@ function App() {
     const distance = hypot / maxHypot;
     const easeDistance = easing(distance);
     setDistance(easeDistance);
-  }
+  };
 
-  const handleMove = ({clientX, clientY}) => {
-    calculateDistance([clientX, clientY])
-  }
+  const handleMove = ({ clientX, clientY }) => {
+    calculateDistance([clientX, clientY]);
+  };
 
-  const handleTouchMove = ({touches}) => {
-    calculateDistance([touches[0].clientX, touches[0].clientY])
-  }
+  const handleTouchMove = ({ touches }) => {
+    calculateDistance([touches[0].clientX, touches[0].clientY]);
+  };
 
   const handleRandomBgClick = () => {
-    setBgColor(Math.floor(Math.random() * 240))
-  }
+    setBgColor(Math.floor(Math.random() * 240));
+  };
 
   const handlePaintingUrlClick = () => {
     if (paintingNum === 6) {
-      setPaintingNum(1)
+      setPaintingNum(1);
     } else {
-      setPaintingNum(paintingNum + 1)
+      setPaintingNum(paintingNum + 1);
     }
-    switch(paintingNum) {
+    switch (paintingNum) {
       case 1:
         // orange hue painting
-        setBgColor(240)
+        setBgColor(240);
         break;
       case 2:
         // black painting
-        setBgColor(330)
+        setBgColor(330);
         break;
       case 3:
         // yellow painting
-        setBgColor(285)
+        setBgColor(285);
         break;
       case 4:
         // greenish-teal painting
-        setBgColor(15)
+        setBgColor(15);
         break;
       case 5:
         // red-yellow-green painting
-        setBgColor(262)
+        setBgColor(262);
         break;
       case 6:
         // reddish-gray painting
-        setBgColor(145)
+        setBgColor(200);
         break;
       default:
-        setBgColor(240)
+        setBgColor(240);
     }
-    const Url = `src/components/ImageSection/static/de_kooning${paintingNum}.jpeg`
-    setPaintingUrl(Url)
-  }
+    const Url = `src/components/ImageSection/static/de_kooning${paintingNum}.jpeg`;
+    setPaintingUrl(Url);
+  };
 
   const toggleModal = () => {
-    setShowModal((showModal) => !showModal)
-  }
+    setShowModal((showModal) => !showModal);
+  };
 
   return (
     <main>
       {showModal && <Modal toggleModal={toggleModal} />}
       <Header />
-      <ButtonContainer> 
+      <ButtonContainer>
         <NextButton next={handlePaintingUrlClick} />
+        <AboutButton action={toggleModal} />
       </ButtonContainer>
       <Footer />
-      <Wrapper
-        onMouseMove={handleMove}
-        onTouchMove={handleTouchMove}
-        $color={bgColor}
-      >
-        <Button onClick={toggleModal}>Sign up for updates</Button>
+      <Wrapper onMouseMove={handleMove} onTouchMove={handleTouchMove} $color={bgColor}>
         <ImageContainer $isTogether={distance < 0.001}>
-          {matrix.map(([x,y], index) => (
+          {matrix.map(([x, y], index) => (
             <ImageBox key={index} x={x} y={y} percent={distance} url={paintingUrl} />
           ))}
         </ImageContainer>
       </Wrapper>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ControlPanel/ButtonContainer.jsx
+++ src/components/ControlPanel/ButtonContainer.jsx
@@ -1,34 +1,36 @@
-import React, { useState } from 'react'
-import './ControlPanel.css'
-import Arrow from './Arrow'
+import React, { useState } from 'react';
+import './ControlPanel.css';
+import Arrow from './Arrow';
 
 const ButtonContainer = (props) => {
-    const [hideContainer, setHideContainer] = useState(false)
-    const [containerPosition, setContainerPosition] = useState('left-0 top-10')
-    const [arrowDirection, setArrowDirection] = useState('left')
-    
-    const handleClick = () => {
-        setHideContainer(!hideContainer);
-        hideContainer ? setContainerPosition('left-0 top-10') : setContainerPosition('container-hide top-10')
-        hideContainer ? setArrowDirection('left') : setArrowDirection('right')
-    }
+  const [hideContainer, setHideContainer] = useState(false);
+  const [containerPosition, setContainerPosition] = useState('left-0 top-10');
+  const [arrowDirection, setArrowDirection] = useState('left');
 
-    let containerStyles = containerPosition + 'transition duration-500 ease-in-out bg-gray-800 shadow-xl w-52 h-20 absolute left-0 top-32 z-10'
+  const handleClick = () => {
+    setHideContainer(!hideContainer);
+    hideContainer
+      ? setContainerPosition('left-0 top-10')
+      : setContainerPosition('container-hide top-10');
+    hideContainer ? setArrowDirection('left') : setArrowDirection('right');
+  };
 
-    return (
-        <div className={containerStyles}>
-            <div className="flex justify-between items-center px-3 h-full">
-                <div className="flex justify-center items-center">
-                    {props.children}
-                </div>
-                <div>
-                    <button onClick={handleClick}>
-                        <Arrow direction={arrowDirection} />
-                    </button>
-                </div>
-            </div>
+  let containerStyles =
+    containerPosition +
+    'transition duration-500 ease-in-out bg-gray-800 shadow-xl w-52 py-4 absolute left-0 top-32 z-10';
+
+  return (
+    <div className={containerStyles}>
+      <div className='flex justify-between items-center px-3 h-full'>
+        <div className='flex flex-col justify-center gap-4'>{props.children}</div>
+        <div>
+          <button onClick={handleClick}>
+            <Arrow direction={arrowDirection} />
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ButtonContainer
+export default ButtonContainer;
